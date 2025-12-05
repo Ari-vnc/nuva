f81a5f650274d291d525efad2ee3ae254d551fda
Ari-vnc <dagnerdev@gmail.com>
2025-12-05 00:51:03 -0300

    nuevos productos con pin y unicornios

--- backend/products.py
+++ backend/products.py
@@ -2,32 +2,48 @@ BASE_URL = ""
 
 products = [
     {
-        "sku": 'ASLN-0001',
-        "title": 'Sandalia | Ojotatipo crocs - negro',
-        "description": 'Sandalia con pines decorativos de stich',
+        "sku": 'ASLN-0002',
+        "title": 'Sandalia stich - negro',
+        "description": 'Sandalia con pines de stich',
         "price": 9500,
-        "category": 'niño',
-        "image": '/img/asln-0001.avif'
+        "category": 'niña',
+        "image": '/img/asln-0002.avif'
     },
     {
-        "sku": 'ASLA-0001',
-        "title": 'Sandalia | Ojota tipo crocs - azul',
-        "description": 'Sandalia con pines decorativos de stich',
+        "sku": 'ASLV-0002',
+        "title": 'Sandalia stich - veige',
+        "description": 'Sandalia con pines de stich',
         "price": 9500,
-        "category": 'niño',
-        "image": '/img/asla-0001.avif'
+        "category": 'niña',
+        "image": '/img/aslv-0002.avif'
     },
     {
-        "sku": 'ASLV-0001',
-        "title": 'Sandalia | Ojota tipo crocs - veige',
-        "description": 'Sandalia con pines decorativos de stich',
+        "sku": 'ASLA-0002',
+        "title": 'Sandalia stich - azul',
+        "description": 'Sandalia con pines de stich',
         "price": 9500,
+        "category": 'niña',
+        "image": '/img/asla-0002.avif'
+    },
+    {
+        "sku": 'COUR-0001',
+        "title": 'Sandalia unicornio - rosa',
+        "description": 'Sandalia de unicornio con franja de colores',
+        "price": 15000,
+        "category": 'niña',
+        "image": '/img/cour-0001.avif'
+    },
+    {
+        "sku": 'COUN-0001',
+        "title": 'Sandalia unicornio - negro',
+        "description": 'Sandalia de unicornio con franja de colores',
+        "price": 15000,
         "category": 'niño',
-        "image": 'img/aslv-0001.avif'
+        "image": '/img/coun-0001.avif'
     },
     {
         "sku": 'BSLA-0001',
-        "title": 'Sandalia jota pez linterna - azul',
+        "title": 'Sandalia | Ojota pez linterna - azul',
         "description": 'Ilimina en 3 colores (rojo, azul y verde)',
         "price": 15000,
         "category": 'niño',
@@ -48,5 +64,29 @@ products = [
         "price": 15000,
         "category": 'niña',
         "image": '/img/bslg-0001.avif'
-    }
+    },
+    {
+        "sku": 'ASLN-0001',
+        "title": 'Sandalia | Ojota tipo Crocs - negro',
+        "description": 'Sandalia con pines decorativos de stich',
+        "price": 9500,
+        "category": 'todas',
+        "image": '/img/asln-0001.avif'
+    },
+    {
+        "sku": 'ASLA-0001',
+        "title": 'Sandalia | Ojota tipo crocs - azul',
+        "description": 'Sandalia con pines decorativos de stich',
+        "price": 9500,
+        "category": 'niño',
+        "image": '/img/asla-0001.avif'
+    },
+    {
+        "sku": 'ASLV-0001',
+        "title": 'Sandalia | Ojota tipo crocs - veige',
+        "description": 'Sandalia con pines decorativos de stich',
+        "price": 9500,
+        "category": 'niño',
+        "image": 'img/aslv-0001.avif'
+    },
 ]
